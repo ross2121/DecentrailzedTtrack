0c7f694f4057cc642dd85996f1bf1f3e70c34fe6
ross2121 <youvalsingh40@gmail.com>
2025-03-14 05:54:44 +0530

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { userrouter } from "./Auth/auth";
+import cors from "cors"
 const app=express();
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1",userrouter);
 const port=3000;
 app.listen(port,()=>{
